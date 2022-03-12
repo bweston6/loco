d397d2aaf30fd1b8335736eeee119fb620c9ea69
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-12 21:46:19 +0000

    Testing github CD with webhooks.

---
+++ backend/loco/server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from os import system
+from flask import Flask, jsonify, request
+
+api = Flask(__name__)
+
+baseurl = "/api"
+users = []
+
+@api.route(baseurl + "/users")
+def getUsers():
+    return jsonify(users)
+
+@api.route(baseurl + "/users", methods=["POST"])
+def addUser():
+    users.append(request.get_json())
+    return "", 204
+
+@api.route(baseurl + "/webhook", methods=["POST"])
+def webook():
+    system(export GIT_SSH_COMMAND='ssh -i /home/ben/.ssh/loco-backend -o IdentitiesOnly=yes'; git fetch; git reset origin/base --hard; git pull)
+    return "ok"
