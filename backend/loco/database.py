e1cb56826f876b402bdd0a7b9702a74464c04d07
Droanox <50217553+Droanox@users.noreply.github.com>
2022-03-23 19:03:16 +0000

    Update database.py

--- backend/loco/database.py
+++ backend/loco/database.py
@@ -4,18 +4,16 @@ from mysql.connector import connect, Error, errorcode
 try:
     cnx = connect(user="root",
                   host=input("host: "),
-                  password=input("password: "),
-                  database="loco")
+                  password=input("password: "))
     cursor = cnx.cursor()
-    cursor.execute("USE {}".format('loco'))
 except Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Incorrect username or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print('"loco" Database does not exist')
     else:
         print(err)
+    exit(1)
 
+DATABASE = 'loco'
 TABLES = {}
 TABLES['users'] = (
     "CREATE TABLE `users` ("
@@ -47,17 +45,41 @@ TABLES['groups'] = (
     "  CONSTRAINT PRIMARY KEY (`group_ID`)"
     ")")
 
-for tableName in TABLES:
-    tableDescription = TABLES[tableName]
+def createDatabase(cursor):
     try:
-        print("creating table {}: ".format(tableName), end='')
-        cursor.execute(tableDescription)
+        cursor.execute("USE {}".format(DATABASE))
     except Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
+        print("Database {} does not exist.".format(DATABASE))
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            try:
+                cursor.execute("CREATE DATABASE {}".format(DATABASE))
+            except Error as err:
+                print("Database creation failed: {}".format(err))
+                exit(1)
+            print("Database {} successfully created".format(DATABASE))
+            cnx.database = DATABASE
         else:
-            print(err.msg)
-    else:
-        print("created.")
+            print(err)
+            exit(1)
+
+def createTables(cursor):
+    for tableName in TABLES:
+        tableDescription = TABLES[tableName]
+        try:
+            print("creating table {}: ".format(tableName), end='')
+            cursor.execute(tableDescription)
+        except Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("created.")
+
+def main():
+    createDatabase(cursor)
+    createTables(cursor)
+
+main()
 cursor.close()
 cnx.close()
