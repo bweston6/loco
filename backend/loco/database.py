87afc5ac3693b92ae8ec1f4fa51ae0c0c87285e3
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-26 18:44:28 +0000

    Changing to socket authentication.

--- backend/loco/database.py
+++ backend/loco/database.py
@@ -1,11 +1,11 @@
-import mysql.connector
 from mysql.connector import connect, Error, errorcode
 
 try:
-    cnx = connect(user="root",
-                  host=input("host: "),
-                  password=input("password: "))
-    cursor = cnx.cursor()
+    conn = connect(
+            host = "localhost",
+            user = "loco",
+            unix_socket = "/var/lib/mysql/mysql.sock")
+    cursor = conn.cursor()
 except Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Incorrect username or password")
@@ -16,34 +16,34 @@ except Error as err:
 DATABASE = 'loco'
 TABLES = {}
 TABLES['users'] = (
-    "CREATE TABLE `users` ("
-    "  `full_name` varchar(100) NOT NULL,"
-    "  `email` varchar(100) NOT NULL,"
-    "  `hostFlag` BIT NOT NULL,"
-    "  CONSTRAINT PRIMARY KEY (`email`)"
-    ")")
+        "CREATE TABLE `users` ("
+        "  `full_name` varchar(100) NOT NULL,"
+        "  `email` varchar(100) NOT NULL,"
+        "  `hostFlag` BIT NOT NULL,"
+        "  CONSTRAINT PRIMARY KEY (`email`)"
+        ")")
 
 TABLES['events'] = (
-    "CREATE TABLE `events` ("
-    "  `event_ID` int NOT NULL,"
-    "  `event_name` varchar(50) NOT NULL,"
-    "  `start_time` datetime NOT NULL,"
-    "  `duration` time NOT NULL,"
-    "  `latitude` decimal(8,6) NOT NULL,"
-    "  `longitude` decimal(9,6) NOT NULL,"
-    "  `radius` int NOT NULL,"
-    "  `description` varchar(1000),"
-    "  `emails` JSON NOT NULL,"
-    "  CONSTRAINT PRIMARY KEY (`event_ID`)"
-    ")")
+        "CREATE TABLE `events` ("
+        "  `event_ID` int NOT NULL,"
+        "  `event_name` varchar(50) NOT NULL,"
+        "  `start_time` datetime NOT NULL,"
+        "  `duration` time NOT NULL,"
+        "  `latitude` decimal(8,6) NOT NULL,"
+        "  `longitude` decimal(9,6) NOT NULL,"
+        "  `radius` int NOT NULL,"
+        "  `description` varchar(1000),"
+        "  `emails` JSON NOT NULL,"
+        "  CONSTRAINT PRIMARY KEY (`event_ID`)"
+        ")")
 
 TABLES['groups'] = (
-    "CREATE TABLE `groups` ("
-    "  `group_ID` int NOT NULL,"
-    "  `group_name` varchar(50) NOT NULL,"
-    "  `emails` JSON NOT NULL,"
-    "  CONSTRAINT PRIMARY KEY (`group_ID`)"
-    ")")
+        "CREATE TABLE `groups` ("
+        "  `group_ID` int NOT NULL,"
+        "  `group_name` varchar(50) NOT NULL,"
+        "  `emails` JSON NOT NULL,"
+        "  CONSTRAINT PRIMARY KEY (`group_ID`)"
+        ")")
 
 def createDatabase(cursor):
     try:
@@ -57,7 +57,7 @@ def createDatabase(cursor):
                 print("Database creation failed: {}".format(err))
                 exit(1)
             print("Database {} successfully created".format(DATABASE))
-            cnx.database = DATABASE
+            conn.database = DATABASE
         else:
             print(err)
             exit(1)
@@ -82,4 +82,4 @@ def main():
 
 main()
 cursor.close()
-cnx.close()
+conn.close()
