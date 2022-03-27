df4d65980b139778fb926336fb7b8394cb501642
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-27 13:04:28 +0100

    Adding test secret to environment.

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 source $(pipenv --venv)/bin/activate
+export SECRET_KEY="b'\xe8\xc6\xd3Oa]=R#\xe7O\xb0\xd2\xdc\xa4g\x835I\xbbS\xf1\n\xd1'"
 hupper -m waitress --port=55580 --call loco:create_app
