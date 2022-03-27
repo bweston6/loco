2f4bc76910a8076595d0fdc4058cc7c5f2eaff9f
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-27 13:19:54 +0100

    Creating tables with main instead of api calls.

--- backend/loco/__init__.py
+++ backend/loco/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 
+import loco.database as db
 from .api import api
 from .web import web
 
 def create_app():
+    """
+    Ensures database tables are created, creates the main flask app and registers blueprints to the main app.
+    :returns: the main flask app
+    """
+    conn = db.openConnection()
+    db.createTables(conn.cursor())
+    db.closeConnection(conn)
     app = Flask(__name__)
     app.register_blueprint(api)
     app.register_blueprint(web)
