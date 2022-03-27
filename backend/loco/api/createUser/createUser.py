f7661bb7dcd5462ef95cf8f2ab09b7a43a6c469c
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-27 13:06:16 +0100

    Testing token generation.

--- backend/loco/api/createUser/createUser.py
+++ backend/loco/api/createUser/createUser.py
@@ -1,12 +1,27 @@
+# being worked on by Ben
+# todo - remove this message
+
+from flask import abort, jsonify, request
+from mariadb import Error
 from loco.api import api
 import loco.database as db
+import loco.auth as auth
 
-@api.route('/createUser')
+@api.route('/createUser', methods=['POST'])
 def createUser():
-    conn = db.openConnection()
-    if conn != None:
+    """
+    Creates a user, generates an authentication token and adds the information to the database.
+    :returns: The generated authentication token or an error page if there has been an error.
+    """
+    try:
+        conn = db.openConnection()
         db.createTables(conn.cursor())
+        requestData = request.get_json()
+        if 'fullName' in requestData and 'email' in requestData and 'hostFlag' in requestData:
+            token = auth.generateToken(requestData['email'])
+        else: 
+            return jsonify(error='missing parameters'), 400
         db.closeConnection(conn)
-    else:
-        return {'database' : 'error'}
-    return {'key' : 'value'}
+        return jsonify(token)
+    except Error:
+        return jsonify(error='database error'), 500
