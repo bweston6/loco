467ace1d961ed247927678e17b4a3b4223a18341
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 00:54:38 +0100

    Update getEvent.py

--- backend/loco/api/getEvent/getEvent.py
+++ backend/loco/api/getEvent/getEvent.py
@@ -2,4 +2,9 @@ from loco.api import api
 
 @api.route('/getEvent')
 def getEvent():
+	"""
+    	Returns the event
+	
+    	:returns: dictionary with the key and value of the event
+    	"""
 	return {'key' : 'value'}
