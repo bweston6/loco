a5f879da7579de5552e7810113f77de8e98b7c11
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-28 11:35:51 +0100

    Update createEvent.py

--- backend/loco/api/createEvent/createEvent.py
+++ backend/loco/api/createEvent/createEvent.py
@@ -2,4 +2,8 @@ from loco.api import api
 
 @api.route('/createEvent')
 def createEvent():
+	"""
+    	Ensures event is created
+    	:returns: the key and value of the event
+    	"""
 	return {'key' : 'value'}
