0dcf245077ecebd580b4d4cd8e7d12813303aa12
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 00:57:10 +0100

    Update setAttendance.py

--- backend/loco/api/setAttendance/setAttendance.py
+++ backend/loco/api/setAttendance/setAttendance.py
@@ -2,4 +2,9 @@ from loco.api import api
 
 @api.route('/setAttendance')
 def setAttendance():
+	"""
+    	Ensures attendance is set
+	
+    	:returns: dictionary with the key and value of the attendance
+    	"""
 	return {'key' : 'value'}
