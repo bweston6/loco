77d6a9ce86be9a3e8a4a88b25133eb0445eead2c
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 00:55:12 +0100

    Update getUser.py

--- backend/loco/api/getUser/getUser.py
+++ backend/loco/api/getUser/getUser.py
@@ -2,4 +2,9 @@ from loco.api import api
 
 @api.route('/getUser')
 def getUser():
+	"""
+    	Returns the user
+	
+    	:returns: dictionary with the key and value of the user
+    	"""
 	return {'key' : 'value'}
