966d1a6c02f99ce688aa162f9dbdd7364d3b3f85
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 00:10:02 +0100

    Update createGroup.py

--- backend/loco/api/createGroup/createGroup.py
+++ backend/loco/api/createGroup/createGroup.py
@@ -2,4 +2,9 @@ from loco.api import api
 
 @api.route('/createGroup')
 def createGroup():
+	"""
+    	Ensures group is created
+    	:returns: the key and value of the group
+	:rtype: dictionary
+    	"""
 	return {'key' : 'value'}
