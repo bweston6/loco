af9027cefd082117eb1d8b25ae23f674a6605cfe
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 00:53:55 +0100

    Update getAttendance.py

--- backend/loco/api/getAttendance/getAttendance.py
+++ backend/loco/api/getAttendance/getAttendance.py
@@ -2,4 +2,9 @@ from loco.api import api
 
 @api.route('/getAttendance')
 def getAttendance():
+	"""
+    	Returns attendance 
+	
+    	:returns: dictionary with the key and value of the attendance
+    	"""
 	return {'key' : 'value'}
