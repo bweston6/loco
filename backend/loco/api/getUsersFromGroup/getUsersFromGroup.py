68a1d61ca5b4f3928605e1cac20df0237face63b
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 00:56:28 +0100

    Update getUsersFromGroup.py

--- backend/loco/api/getUsersFromGroup/getUsersFromGroup.py
+++ backend/loco/api/getUsersFromGroup/getUsersFromGroup.py
@@ -2,4 +2,9 @@ from loco.api import api
 
 @api.route('/getUsersFromGroup')
 def getUsersFromGroup():
+	"""
+    	Returns the users from the selected group
+	
+    	:returns: dictionary with the key and value of the user from a specific group
+    	"""
 	return {'key' : 'value'}
