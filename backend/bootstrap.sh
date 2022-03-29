a44921691f30eb6bcff7fd04deacb0c192970a48
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-29 14:57:27 +0100

    Adding email details.

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 source $(pipenv --venv)/bin/activate
+# todo - remove envvars from here
 export SECRET_KEY="***REMOVED***"
+export EMAIL="***REMOVED***"
+export KEY="***REMOVED***"
 hupper -m waitress --port=55580 --call loco:create_app
