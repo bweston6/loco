2e6363975e18df8b486273945675f58a674a9c7e
Droanox <50217553+Droanox@users.noreply.github.com>
2022-03-29 17:27:16 +0100

    adding and updating comments
    gives the sql query for inserting into the   tables

--- backend/loco/api/createEvent/createEvent.py
+++ backend/loco/api/createEvent/createEvent.py
@@ -10,3 +10,11 @@ def createEvent():
     	:returns: dictionary with the key and value of the event
     	"""
 	return {'key' : 'value'}
+
+#   "outline for the data insertion data can be of any type"
+#   addEvents = ("INSERT INTO events"
+#              "(event_ID, event_name, start_time, duration, latitude, longitude, radius, description, emails)"
+#              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+#   dataEvents = (data, data, data, data, data, data, data, data, data)
+#   cursor.execute(addUser, dataUser)
+#   conn.commit()
--- backend/loco/api/createGroup/createGroup.py
+++ backend/loco/api/createGroup/createGroup.py
@@ -8,3 +8,11 @@ def createGroup():
     	:returns: dictionary with key and value of the group
     	"""
 	return {'key' : 'value'}
+
+#   "outline for the data insertion data can be of any type"
+#   addGroup = ("INSERT INTO group"
+#              "(group_id, group_name, emails)"
+#              "VALUES (%s, %s, %s)")
+#   dataGroup = (data, data, data)
+#   cursor.execute(addGrouo, dataGroup)
+#   conn.commit()
