1d451da52a88cf15f2be095bfe547a146415d83b
nouisl <78652710+nouisl@users.noreply.github.com>
2022-03-29 01:56:42 +0100

    Update __init__.py

--- backend/loco/web/__init__.py
+++ backend/loco/web/__init__.py
@@ -4,4 +4,9 @@ web = Blueprint('web', __name__, static_folder='static')
 
 @web.route('/')
 def index():
+	"""
+    Description
+    
+    :returns: 
+    """
     return web.send_static_file('index.html')
