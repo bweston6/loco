dca0817199559dbf2f8380955af2d0dfa8531f34
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-29 14:58:59 +0100

    Adding email authentication.

--- backend/loco/api/createUser/createUser.py
+++ backend/loco/api/createUser/createUser.py
@@ -1,7 +1,7 @@
 # being worked on by Ben
 # todo - remove this message
 
-from flask import abort, jsonify, request
+from flask import jsonify, request
 from mariadb import Error
 from loco.api import api
 import loco.database as db
@@ -11,17 +11,21 @@ import loco.auth as auth
 def createUser():
     """
     Creates a user, generates an authentication token and adds the information to the database.
-	
+
     :returns: The generated authentication token or an error page if there has been an error.
     """
     try:
         conn = db.openConnection()
         requestData = request.get_json()
-        if 'fullName' in requestData and 'email' in requestData and 'hostFlag' in requestData:
-            token = auth.generateToken(requestData['email'])
+        if 'OTP' in requestData and 'fullName' in requestData and 'email' in requestData and 'hostFlag' in requestData:
+            token = auth.generateToken(int(requestData['OTP']), requestData['email'])
         else: 
             return jsonify(error='missing parameters'), 400
         db.closeConnection(conn)
         return jsonify(token)
-    except Error:
+    except KeyError as e:
+        return jsonify(error='email not authenticated'), 400
+    except ValueError as e:
+        return jsonify(error='invalid OTP'), 400
+    except Error as e:
         return jsonify(error='database error'), 500
