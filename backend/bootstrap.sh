a3f0d8ea83f8eaf829b0fb62074d2aecf1e9e420
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-30 18:11:02 +0100

    Passing arguments to hupper.

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -4,4 +4,4 @@ source $(pipenv --venv)/bin/activate
 export SECRET_KEY="***REMOVED***"
 export EMAIL="***REMOVED***"
 export KEY="***REMOVED***"
-hupper -m waitress --port=55580 --call loco:create_app
+hupper -m waitress --port=55580 --call loco:create_app "$@"
