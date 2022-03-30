535dbfe52681c7283dec50fcd33906e7ecbff527
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-03-30 18:11:02 +0100

    Passing arguments to hupper.

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -4,4 +4,4 @@ source $(pipenv --venv)/bin/activate
 export SECRET_KEY="b'\xe8\xc6\xd3Oa]=R#\xe7O\xb0\xd2\xdc\xa4g\x835I\xbbS\xf1\n\xd1'"
 export EMAIL="b.weston60@gmail.com"
 export KEY="pwarsbtwojkaiuci"
-hupper -m waitress --port=55580 --call loco:create_app
+hupper -m waitress --port=55580 --call loco:create_app "$@"
