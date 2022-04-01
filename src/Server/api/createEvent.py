72513646aaf79abcfabd7f9ef5ede8dd7ed68228
nouisl <78652710+nouisl@users.noreply.github.com>
2022-04-01 04:55:57 +0100

    Update createEvent.py
    code & comment ; not tested yet

--- src/Server/api/createEvent.py
+++ src/Server/api/createEvent.py
@@ -1,20 +1,76 @@
 # being worked on by Noushin
 
+from flask import jsonify, request
+from mariadb import Error
 from . import api
+from .. import auth, database as db
+import logging
 
 @api.route('/createEvent')
 def createEvent():
-	"""
-    	Ensures event is created
-	
-    	:returns: dictionary with the key and value of the event
+	 """Creates an event and adds the information to the database.
+    
+    	:<json int eventId: 
+    	:<json string eventName: 
+    	:<json number startTime: 
+    	:<json number duration: 
+	:<json number locationLong: 
+    	:<json number locationLat: 
+	:<json number radius: 
+	:<json string description: 
+	:<json array email: 
+   
+    	:>json string error: An error message if the action cannot complete
+    
+    	:statuscode 200: Operation completed successfully
+    	:statuscode 400: JSON parameters are missing
+    	:statuscode 500: Server database error
     	"""
-	return {'key' : 'value'}
+	try:
+		requestData = request.get_json()
+        	if 'eventId' in requestData and 'eventName' in requestData and 'startTime' in requestData and 'duration' in requestData 
+		and 'locationLong' in requestData and 'locationLat' in requestData and 'radius' in requestData 
+		and 'description' in requestData and 'email' in requestData:
+			conn = db.openConnection()
+			cursor = conn.cursor()
+			addEvent = ("""REPLACE INTO event (
+			event_id, 
+			event_name, 
+			start_time, 
+			duration, 
+			longitude,
+			latitude,
+			radius,
+			description,
+			emails
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+            )
+            eventData = (
+                requestData['eventId'],
+                requestData['eventName'],
+		requestData['startTime'],
+		requestData['duration'],
+		requestData['locationLong'],
+		requestData['locationLat'],
+		requestData['radius'],
+		requestData['description'],
+		requestData['email']
+            )
+            cursor.execute(addEvent, eventData)
+            conn.commit()
+            db.closeConnection(conn)
+            return jsonify(eventData), 200
+        else: 
+            return jsonify(error='missing parameters'), 400
+    	except Error as e:
+        logging.error(e)
+        return jsonify(error='database error'), 500
+
 
 #   "outline for the data insertion data can be of any type"
 #   addEvents = ("INSERT INTO events "
 #              "(event_ID, event_name, start_time, duration, latitude, longitude, radius, description, emails) "
 #              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
 #   dataEvents = (data, data, data, data, data, data, data, data, data)
-#   cursor.execute(addUser, dataUser)
-#   conn.commit()
+
