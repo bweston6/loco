f13ddb3ad6f03570a5de182e5cd41b4b5befc632
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-04-02 15:44:33 +0100

    Only updating if tests pass.

--- src/Server/api/__init__.py
+++ src/Server/api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from os import system
 
 api = Blueprint('api', __name__, url_prefix='/api')
@@ -15,10 +15,17 @@ from . import setAttendance
 
 @api.route("/webhook", methods=["POST"])
 def webook():
-    """Updates the repository to the HEAD of the base branch, triggering a restart.
-   
-    :return: "ok" to indicate success.
-    :rtype: String
+    """Updates the repository to the HEAD of the base branch triggering a restart, provided that tests pass.
+    :<json str action: The current status of the workflow
+    :<json str workflow.name: The name of the workflow
+
+    :return: A status message to indicate the action taken
+    :rtype: str
+
+    :status 200: Request completed or ignored successfully
     """
-    system("git fetch; git reset origin/base --hard; git pull")
-    return "ok"
+    requestData = request.get_json()
+    if requestData['action'] == "completed" and requestData['workflow']['name'] == "Testing":
+        system("git fetch; git reset origin/base --hard; git pull")
+        return "updated to HEAD", 200
+    return "request ignored", 200
