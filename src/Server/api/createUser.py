0bd202ffc236d320d3f4ca6756139ca591472468
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-04-03 17:01:02 +0100

    Making status codes more descriptive and correcting datatypes.

--- src/Server/api/createUser.py
+++ src/Server/api/createUser.py
@@ -1,10 +1,10 @@
 # being worked on by Ben
 # todo - remove this message
 
-from flask import jsonify, request
-from mariadb import Error
 from . import api
 from .. import auth, database as db
+from flask import jsonify, request
+from mariadb import Error
 import logging
 
 @api.route('/createUser', methods=['POST'])
@@ -12,16 +12,16 @@ def createUser():
     """Creates a user, generates an authentication token and adds the information to the database.
     
     :<json string OTP: The one time password associated with the ``email`` you want to register. This is received by calling :http:post:`/api/authenticateEmail`
-    :<json string fullName: The full name of the user you want to register
-    :<json string email: The email you want to register under (must have a registered OTP using :http:post:`/api/authenticateEmail`)
+    :<json str fullName: The full name of the user you want to register
+    :<json str email: The email you want to register under (must have a registered OTP using :http:post:`/api/authenticateEmail`)
     :<json bool hostFlag: Defines where the user is a host or an attendee 
 
-    :>json string token: The authentication ``token`` for the new user
-    :>json string error: An error message if the action cannot complete
+    :>json str token: The authentication ``token`` for the new user
+    :>json str error: An error message if the action cannot complete
     
     :statuscode 200: Operation completed successfully
     :statuscode 400: JSON parameters are missing
-    :statuscode 401: JSON parameters are invalid, see ``error``
+    :statuscode 401: JSON authentication parameters are invalid, see ``error``
     :statuscode 500: Server database error
     """
     try:
@@ -44,7 +44,6 @@ def createUser():
                 token,
                 requestData['hostFlag']
             )
-            print(token)
             cursor.execute(addUser, userData)
             conn.commit()
             db.closeConnection(conn)
