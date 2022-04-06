ce63c9ca05afaa38e0af21a63c8279925bef2e16
Gunan <31938127+gunansingh@users.noreply.github.com>
2022-04-06 14:48:14 +0100

    Update setAttendance
    -

--- src/Server/api/setAttendance.py
+++ src/Server/api/setAttendance.py
@@ -1,4 +1,11 @@
+#Gunan
+#incomplete still
+
 from . import api
+from .. import auth, database as db
+from flask import jsonify, request
+from mariadb import Error
+import logging
 
 @api.route('/setAttendance')
 def setAttendance():
@@ -7,4 +14,20 @@ def setAttendance():
 	
     	:returns: dictionary with the key and value of the attendance
     	"""
-	return {'key' : 'value'}
+    try:
+        requestData = request.get_json()
+        if ('token' in requestData and 'email' in requestData and 'eventID' in requestData):
+            q1 = (""" UPDATE user
+                SET attendance = "True"
+                WHERE eventID = ?
+            """)
+            conn = db.openConnection()
+            cursor = conn.cursor()
+            cursor.execute(q1, (requestData['EventID'], ))
+        
+        else:
+            return jsonify(error='missing parameters'), 400      
+    
+    except Error as e:
+        logging.error(e)
+        return jsonify(error='database error'), 500
