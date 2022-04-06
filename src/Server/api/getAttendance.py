2d83c1f4adb885adc78498a8ecf9208853a34925
Gunan <31938127+gunansingh@users.noreply.github.com>
2022-04-06 14:46:21 +0100

    Update getAttendance.py
    probably doesn't work

--- src/Server/api/getAttendance.py
+++ src/Server/api/getAttendance.py
@@ -1,4 +1,11 @@
+#Gunan
+#mans on it still
+
 from . import api
+from .. import auth, database as db
+from flask import jsonify, request
+from mariadb import Error
+import logging
 
 @api.route('/getAttendance')
 def getAttendance():
@@ -7,4 +14,23 @@ def getAttendance():
 	
     	:returns: dictionary with the key and value of the attendance
     	"""
-	return {'key' : 'value'}
+    try:
+        requestData = request.get_json()
+        if ('token' in requestData and 'email' in requestData and 'eventID' in requestData):
+            q1 = (""" SELECT attendance 
+                FROM Users
+                WHERE token = ?
+            """)
+            conn = db.openConnection()
+            cursor = conn.cursor()
+            cursor.execute(q1, (requestData['token'], ))
+            attendance = cursor.fetchone()[0]
+            
+            return attendance
+        
+        else:
+            return jsonify(error='missing parameters'), 400      
+    
+    except Error as e:
+        logging.error(e)
+        return jsonify(error='database error'), 500
