c47d589f1cb4d368c7f5f08dac4a942061138846
Timi <70448055+TimiB54@users.noreply.github.com>
2022-04-07 08:53:04 +0100

    Fixed indentation and tabs

--- src/Server/api/getEvent.py
+++ src/Server/api/getEvent.py
@@ -19,49 +19,48 @@ def getEvent():
 
     :statuscode 200: Operation completed successfully
     :statuscode 400: JSON parameters are missing
-	:statuscode 401: Invalid authentication token
+    :statuscode 401: Invalid authentication token
     :statuscode 500: Server database error
     """
     try:
         requestData = request.get_json()
-        if ('eventId' in requestData and 
-                'token' in requestData):
-            
-			# validation of token
-			query1 = ("""SELECT EXISTS (
-                SELECT *
-                FROM events
-                WHERE token = ?
-                LIMIT 1
-            )
-            """)
-			
-			query2 = ("""SELECT *
-                    FROM events
-                    WHERE eventId = ?
+	if ('eventId' in requestData and 'token' in requestData):
+		# validation of token
+		query1 = ("""SELECT EXISTS (
+                	SELECT *
+                	FROM events
+                	WHERE token = ?
+                	LIMIT 1
+            	)
+            	""")
+		
+		query2 = ("""SELECT *
+			FROM events
+			WHERE eventId = ?
                 """)
-            conn = db.openConnection()
-            cursor = conn.cursor()
-            cursor.execute(query1, (requestData['token'], ))
-            tokenValid = cursor.fetchone()[0]
+		
+            	conn = db.openConnection()
+            	cursor = conn.cursor()
+            	cursor.execute(query1, (requestData['token'], ))
+            	tokenValid = cursor.fetchone()[0]
 			
-			if tokenValid == 1:
-                cursor.execute(query2, (requestData['eventId'], ))
-                event = cursor.fetchone()
-                event = {"eventId": event[0],
+		if tokenValid == 1:
+                	cursor.execute(query2, (requestData['eventId'], ))
+                	event = cursor.fetchone()
+                	event = {"eventId": event[0]
                         }
-                return jsonify(event), 200
+                	return jsonify(event), 200
             
-			else:
-                db.closeConnection(conn)
-                raise jwt.InvalidTokenError
+		else:
+                	db.closeConnection(conn)
+                	raise jwt.InvalidTokenError
 			
-        else:
-            return jsonify(error='missing parameters'), 400
+	else:
+		return jsonify(error='missing parameters'), 400
     
-	except jwt.InvalidTokenError as e:
-        logging.info('User query attempted with invalid token')
+    except jwt.InvalidTokenError as e:
+	logging.info('User query attempted with invalid token')
         return jsonify(error='invalid token'), 401
-	except Error as e:
+    except Error as e:
         logging.error(e)
         return jsonify(error='database error'), 500
