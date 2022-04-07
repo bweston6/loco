fe614c26c29c36d5b6fa0d6f5549b43472518bbd
Timi <70448055+TimiB54@users.noreply.github.com>
2022-04-07 08:36:30 +0100

    Updating to only receive token and event id

--- src/Server/api/getEvent.py
+++ src/Server/api/getEvent.py
@@ -13,53 +13,55 @@ def getEvent():
     """Gets event from database
 
     :<json number eventId: The event id.
-    :<json string eventName: The event name that you want to get.
-    :<json number startTime: The starting time of the event, after the attendance has been taken
-    :<json number duration: The duration of an event.
-    :<json number locationLong: The longitute coordinate of an event's location.
-    :<json number locationLat: The latitude coordinate of an event's location.
-    :<json number radius: The radius around an event's coordinates where attendance is accepted.
-    :<json string description: Get a description of the event for users.
-    :<json string email: A list of emails signed up that are expected to attend the event.
+    :<json str token: A valid authentication token (see :http:post:`/api/createUser`)
 
     :>json string error: An error message if the action cannot complete
 
     :statuscode 200: Operation completed successfully
     :statuscode 400: JSON parameters are missing
+	:statuscode 401: Invalid authentication token
     :statuscode 500: Server database error
     """
     try:
         requestData = request.get_json()
         if ('eventId' in requestData and 
-                'eventName' in requestData and
-                'startTime' in requestData and
-                'duration' in requestData and
-                'locationLong' in requestData and
-                'locationLat' in requestData and
-                'radius' in requestData and
-                'description' in requestData and
-                'emails' in requestData):
-            query = ("""SELECT *
+                'token' in requestData):
+            
+			# validation of token
+			query1 = ("""SELECT EXISTS (
+                SELECT *
+                FROM events
+                WHERE token = ?
+                LIMIT 1
+            )
+            """)
+			
+			query2 = ("""SELECT *
                     FROM events
                     WHERE eventId = ?
                 """)
             conn = db.openConnection()
             cursor = conn.cursor()
-            cursor.execute(query, (requestData['eventId'], ))
-            event = cursor.fetchone()
-            event = {"eventId": event[0],
-                    "eventName": event[1],
-                    "startTime": event[2],
-                    "duration": event[3],
-                    "locationLong": event[4],
-                    "locationLat": event[5],
-                    "radius": event[6],
-                    "description": event[7],
-                    "emails": event[8]
-            }
-            return jsonify(event), 200
+            cursor.execute(query1, (requestData['token'], ))
+            tokenValid = cursor.fetchone()[0]
+			
+			if tokenValid == 1:
+                cursor.execute(query2, (requestData['eventId'], ))
+                event = cursor.fetchone()
+                event = {"eventId": event[0],
+                        }
+                return jsonify(event), 200
+            
+			else:
+                db.closeConnection(conn)
+                raise jwt.InvalidTokenError
+			
         else:
             return jsonify(error='missing parameters'), 400
-    except Error as e:
+    
+	except jwt.InvalidTokenError as e:
+        logging.info('User query attempted with invalid token')
+        return jsonify(error='invalid token'), 401
+	except Error as e:
         logging.error(e)
         return jsonify(error='database error'), 500
