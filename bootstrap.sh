5577f1bb2255b7975548842354b9633026fc760a
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-04-14 16:51:00 +0100

    Reload once when building documentation

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ export SECRET_KEY="b'\xe8\xc6\xd3Oa]=R#\xe7O\xb0\xd2\xdc\xa4g\x835I\xbbS\xf1\n\x
 export EMAIL="b.weston60@gmail.com"
 export KEY="pwarsbtwojkaiuci"
 cd src
-hupper -m waitress --port=55580 Server:create_app
+hupper -m waitress --port=55580 Server:app
