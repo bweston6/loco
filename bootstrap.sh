5070992cc9e90cb6ea014dd5da3c46a2f5e3de98
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-04-14 16:53:21 +0100

    Build documentation on first run.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source $(pipenv --venv)/bin/activate
+# building documentation
+make -C docs html latexpdf
 # todo - remove envvars from here
 export SECRET_KEY="b'\xe8\xc6\xd3Oa]=R#\xe7O\xb0\xd2\xdc\xa4g\x835I\xbbS\xf1\n\xd1'"
 export EMAIL="b.weston60@gmail.com"
