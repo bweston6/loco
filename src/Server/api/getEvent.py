194ca12caf91f55f798525a0e7e874b179be8d9e
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-04-16 20:25:32 +0100

    Fixing docstrings.

--- src/Server/api/getEvent.py
+++ src/Server/api/getEvent.py
@@ -16,9 +16,16 @@ def getEvent():
     :<json str token: A valid authentication token (see :http:post:`/api/createUser`)
     :<json number eventId: The ``eventID`` you want to get the details of
 
-    :<json int groupID: The group's unique ID
-    :<json string groupName: The group's name
-    :<json string[] emails: optional All the users emails that are in the group. Only returned if a *host* ``token`` is provided
+    :>json int eventID: The group's unique ID
+    :>json string eventName: The group's name
+    :>json int startTime: The date and time the event is expected to start as a Unix timestamp: (``1649329200`` for ``Thu Apr 07 2022 11:00:00 UTC+0000``)
+    :>json int duration: The duration of an event in Unix millis since the ``startTime``
+    :>json float locationLong: The longitudinal coordinate of an event's location as a fixed point decimal in the format: ``±99.999999``
+    :>json float locationLat: The latitudinal coordinate of an event's location as a fixed point decimal in the format: ``±999.999999``
+    :>json int radius: The radius around an event's coordinates where attendance is accepted in meters
+    :>json str description: A description of the event for users. Maximum of 1000 characters
+    :>json str hostEmail: The email of the host of the event. This email must have an account created by using :http:post:`/api/createUser`
+    :>json string[] emails: optional All the users emails that are in the group. Only returned if a *host* ``token`` is provided
     :>json string error: optional An error message if the action cannot complete
 
     :statuscode 200: Operation completed successfully
