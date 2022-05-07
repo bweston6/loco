393f61cc1c0fe55b12dd479ad4e1198d56dabd71
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-05-07 22:47:18 +0100

    Writing auth tests.

--- src/Server/tests/test_auth.py
+++ src/Server/tests/test_auth.py
@@ -1,26 +1,40 @@
 from Server import auth
+from freezegun import freeze_time
+import Server.database as db
+import datetime
+import jwt
+import os
 
-# SECRET_KEY = os.getenv("SECRET_KEY", None)
-# EMAIL = os.getenv("EMAIL", None)
-# KEY = os.getenv("KEY", None)
-
-
-def test_generateToken():
-    assert True
-
-
-def test_decodeToken():
-    assert True
+SECRET_KEY = os.getenv("SECRET_KEY", None)
 
 
-def test_authenticateEmail():
-    testEmail = "drop@bweston.uk"
-    assert auth.authenticateEmail(testEmail) == True
+@freeze_time("2000-09-06")
+def test_generateToken(hostOTP, hostEmail):
+    payload = jwt.decode(
+        auth.generateToken(hostOTP, hostEmail), SECRET_KEY, algorithms=["HS256"]
+    )
+    print(type(payload["iat"]))
+    print(payload["iat"])
+    print(payload["sub"])
+    assert payload["iat"] == datetime.datetime(2000, 9, 6).timestamp()
+    assert payload["sub"] == hostEmail
 
 
-def test_checkHostEmail():
-    assert True
+def test_decodeToken(host):
+    assert auth.decodeToken(host["token"]) == host["email"]
 
 
-def test_checkHostToken():
-    assert True
+def test_authenticateEmail(hostEmail):
+    assert auth.authenticateEmail(hostEmail) == True
+
+
+def test_checkHostEmail(host, attendee, conn):
+    cursor = conn.cursor()
+    assert auth.checkHostEmail(host["email"], cursor) == True
+    assert auth.checkHostEmail(attendee["email"], cursor) == False
+
+
+def test_checkHostToken(host, attendee, conn):
+    cursor = conn.cursor()
+    assert auth.checkHostToken(host["token"], cursor) == True
+    assert auth.checkHostToken(attendee["token"], cursor) == False
