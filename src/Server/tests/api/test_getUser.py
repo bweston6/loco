348d9204a6a6e5454f2732ba5c1a239ba814dc16
Ben Weston <35574350+bweston6@users.noreply.github.com>
2022-05-09 13:43:48 +0100

    Adding testing stubs.

---
+++ src/Server/tests/api/test_getUser.py
@@ -0,0 +1,34 @@
+def test_getAttendee(client):
+    assert True
+
+
+def test_getAttendee_fromHost(client):
+    assert True
+
+
+def test_getAttendee_fromOtherAttendee(client):
+    assert True
+
+
+def test_getHost(client):
+    assert True
+
+
+def test_getHost_fromAttendee(client):
+    assert True
+
+
+def test_getHost_fromOtherHost(client):
+    assert True
+
+
+def test_getUser_invalidEmail(client):
+    assert True
+
+
+def test_getUser_invalidToken(client):
+    assert True
+
+
+def test_getUser_missingParameters(client):
+    assert True
