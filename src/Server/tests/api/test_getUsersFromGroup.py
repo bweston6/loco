9e5268a55472100784760b17f0c6f1333dc27c4e
Droanox <50217553+Droanox@users.noreply.github.com>
2022-05-10 19:00:33 +0100

    adding 3 tests

---
+++ src/Server/tests/api/test_getUsersFromGroup.py
@@ -0,0 +1,26 @@
+def test_getUsersFromGroup(host, group, client):
+    response = client.post(
+        "/api/getUsersFromGroup",
+        json={
+            "token": host["token"],
+            "groupID": group["groupID"],
+        },
+    )
+    assert response.json["emails"] == group["emails"]
+
+def test_getUsersFromGroup_missingParameters(client):
+    response = client.post(
+        "/api/getUsersFromGroup",
+        json={},
+    )
+    assert response.json["error"] == "missing parameters"
+
+def test_getUsersFromGroup_invalidToken(host, group, client):
+    response = client.post(
+        "/api/getUsersFromGroup",
+        json={
+            "token": "",
+            "groupID": group["groupID"]
+        },
+    )
+    assert response.json["error"] == "invalid token"
