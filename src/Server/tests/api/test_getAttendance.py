f62dba28898596f4da8416e56768448cd67c2cd1
Gunan <31938127+gunansingh@users.noreply.github.com>
2022-05-11 13:13:46 +0100

    testing for get attendance

--- src/Server/tests/api/test_getAttendance.py
+++ src/Server/tests/api/test_getAttendance.py
@@ -1,1 +1,26 @@
+def test_getAttendance(host,attendee,events,client,attaendance):
+    response = client.post(
+        "/api/getAttendance",
+        json={
+            "token": attendee["token"],
+            "email": attendee["email"],
+            "eventID": events["eventID"]
+        },
+    )
+    assert response.json["attendance"] == attendance["attendance_flag"]
+    
+    
+    
+def test_getAttendance_missingParameters(client):
+    response = client.post(
+        "/api/getAttendance",
+        json={},
+    )
+    assert response.json["error"] == "missing parameters"
 
+    
+def test_getAttendance_databaseError(host,client):
+    response = client.post(
+        "/api/getAttendance",
+    )
+    assert response.json["error"] == "database error"
