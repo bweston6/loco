44fc7e5fb94a3ed513529898582d9a9711a25634
nouisl <78652710+nouisl@users.noreply.github.com>
2022-05-11 16:55:27 +0100

    extra testing

--- src/Server/tests/api/test_getAttendance.py
+++ src/Server/tests/api/test_getAttendance.py
@@ -1,26 +1,39 @@
-def test_getAttendance(attendee, events, attendance, client):
+def test_getAttendance(attendee, host, events, attendances, client):
     response = client.post(
         "/api/getAttendance",
         json={
             "token": attendee["token"],
             "email": attendee["email"],
-            "eventID": events["eventID"]
+            "eventID": events["eventID"],
         },
     )
     assert response.json["attendance"] == attendance["attendance_flag"]
     
-    
-    
-def test_getAttendance_missingParameters(client):
+def test_getAttendance_missingParameters(attendee, host, events, attendances, client):
     response = client.post(
         "/api/getAttendance",
         json={},
     )
     assert response.json["error"] == "missing parameters"
 
-    
-def test_getAttendance_databaseError(host,client):
+def test_getAttendance_invalidToken(attendee, host, events, attendances, client):
     response = client.post(
         "/api/getAttendance",
+        json={
+            "token": "test",
+            "email": attendee["email"],
+            "eventID": events["eventID"],
+        },
     )
-    assert response.json["error"] == "database error"
+    assert response.json["error"] == "invalid token"
+
+def test_getAttendance_invalidEventID(attendee, host, events, attendances, client):
+    response = client.post(
+        "/api/getEvent",
+        json={
+            "token": attendee["token"],
+            "email": attendee["email"],
+            "eventID": 0000000000000,
+        },
+    )
+    assert response.json["error"] == "invalid event ID"
