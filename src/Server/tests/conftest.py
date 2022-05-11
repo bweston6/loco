554f52393f4cc541165d5fd57f9399d00b74cfd8
nouisl <78652710+nouisl@users.noreply.github.com>
2022-05-11 21:59:36 +0100

    test 1

--- src/Server/tests/conftest.py
+++ src/Server/tests/conftest.py
@@ -430,6 +430,7 @@ def attendances(conn, events, attendee):
             """
     attendanceData = (attendance["eventID"], attendance["attendeeEmail"])
     cursor.execute(setAttendance, attendanceData)
+    conn.commit()
     cursor.execute(getAttendance, attendanceData)
-    attendance["attendanceFlag"] = cursor.fetchone()[0]
+    attendance["attendanceFlag"] = cursor.fetchall()
     return attendance
